4ba5776684e2c8b9643d4caa09fcccc3eb54cc54
Tomasz Cybulski <jooozek@live.com>
2019-07-09 20:10:00 +0200

    add docs

---
+++ docs/precache-manifest.822f7899bea01afb6b084c91b7ee5599.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186e06bd2497ed94fad335f0add42da7",
+    "url": "/alegria-live/bcs_test/index.html"
+  },
+  {
+    "revision": "b91088e7de8ea9ae7041",
+    "url": "/alegria-live/bcs_test/static/css/main.e9936e47.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/alegria-live/bcs_test/static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "b91088e7de8ea9ae7041",
+    "url": "/alegria-live/bcs_test/static/js/main.dc643e90.chunk.js"
+  },
+  {
+    "revision": "ed69affafde1cbc2e9c0",
+    "url": "/alegria-live/bcs_test/static/js/runtime~main.92406d3a.js"
+  }
+]);
