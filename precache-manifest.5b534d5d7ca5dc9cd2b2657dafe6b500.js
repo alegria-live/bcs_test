1ae886fc65cb23b2a16a3f8f4e6f4ce5bda2aeeb
Tomasz Cybulski <jooozek@live.com>
2019-07-15 13:09:00 +0200

    Updates

--- precache-manifest.5b534d5d7ca5dc9cd2b2657dafe6b500.js
+++ precache-manifest.5b534d5d7ca5dc9cd2b2657dafe6b500.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c18744c089447a3a6e6d6caf6d91f109",
+    "revision": "80c0a2bbd4ea0f8a37199d9fb4e9e192",
     "url": "/bcs_test/index.html"
   },
   {
-    "revision": "75f75f37cc10ecb2bc9f",
-    "url": "/bcs_test/static/css/main.26fe0e42.chunk.css"
+    "revision": "f77203c849c67a5d8556",
+    "url": "/bcs_test/static/css/main.9e923354.chunk.css"
   },
   {
     "revision": "0c9849192e2c5c2300e7",
     "url": "/bcs_test/static/js/2.b9d8976f.chunk.js"
   },
   {
-    "revision": "75f75f37cc10ecb2bc9f",
-    "url": "/bcs_test/static/js/main.a2d1413b.chunk.js"
+    "revision": "f77203c849c67a5d8556",
+    "url": "/bcs_test/static/js/main.c8bf9a69.chunk.js"
   },
   {
     "revision": "19d019f6550ea6f063bc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bcs_test/precache-manifest.29da1f7d65e3af40b95c080177ea59a3.js"
+  "/bcs_test/precache-manifest.5b534d5d7ca5dc9cd2b2657dafe6b500.js"
 );
 
 self.addEventListener('message', (event) => {
